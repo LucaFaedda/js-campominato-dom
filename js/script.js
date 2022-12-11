2f1690d8bcb819782fff272679053992b1bbb738
LucaFaedda <lucakarl91@gmail.com>
2022-12-11 16:01:31 +0100

    tentativi

--- js/script.js
+++ js/script.js
@@ -8,25 +8,82 @@ function generateSquare()
     return quadrato
 }
 
+function generateRandomNumber ( min, max){
 
-const play = document.getElementById('chek-me')
+    let flag = false
+    let randomInt
 
-play.addEventListener('click', function(){
+    while (!flag) {
+        randomInt = Math.floor(Math.random() * (max - min + 1)) + min
+        if(!bombs.includes(randomInt)){
+            flag = true
+        }
+    }
+    return randomInt
+}
+
+function createBombsArray(){
+    let bombs = []
+    for(i = 0; i < 16 ; i++){
+        let random_number = generateRandomNumber(bombs, 1, number)
+        bombs.push(random_number)
+    }
+
+    return bombs
+    
+}
+
+let arrayBombs = []
+const play = document.getElementById('seleziona')
+const number = 0
+
+play.addEventListener('change', function(){
     const grid = document.getElementById('grind') // richiamo da html grid
     
     grid.innerHTML = ""
     
-    for (let i =1 ; i <= 100 ; i++){
-    
-        const number = i
-        const box = generateSquare() // qua mi sono creato i vari square
-        grid.appendChild(box) // creo un figlio all'interno dell'elemento che vado a selezionare
-        box.innerHTML += number
-        box.addEventListener('click', function(){
-            box.classList.add('yellow')
-            console.log(number)
-        })
+    if (this.value == "100"){
+        for (let number  = 1 ; number <= 100 ; number++){
+            
+            arrayBombs = createBombsArray(number)
+            console.log(arrayBombs)
+            const box = generateSquare() // qua mi sono creato i vari square
+            grid.appendChild(box) // creo un figlio all'interno dell'elemento che vado a selezionare
+            box.innerHTML += number
+            box.addEventListener('click', function(){
+                box.classList.add('yellow')
+                console.log(number)
+            })
+            
+        }
+    }
+    else if(this.value == "81"){
+        for (let number =1 ; number <= 81 ; number++){
         
+            const box = generateSquare() // qua mi sono creato i vari square
+            grid.appendChild(box) // creo un figlio all'interno dell'elemento che vado a selezionare
+            box.innerHTML += number
+            box.classList.add('medium')
+            box.addEventListener('click', function(){
+                box.classList.add('yellow')
+                console.log(number)
+            })
+            
+        }
+    }
+    else{
+        for (let number = 1 ; number <= 49 ; number++){
+        
+            const box = generateSquare() // qua mi sono creato i vari square
+            grid.appendChild(box) // creo un figlio all'interno dell'elemento che vado a selezionare
+            box.innerHTML += number
+            box.classList.add('hard')
+            box.addEventListener('click', function(){
+                box.classList.add('yellow')
+                console.log(number)
+            })
+            
+        }
     }
     
 })
