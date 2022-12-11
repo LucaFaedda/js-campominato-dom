e0e37521d1c786659bd625074245a3242ff71f47
LucaFaedda <lucakarl91@gmail.com>
2022-12-11 16:47:13 +0100

    completato fino al blocco caselle

--- js/script.js
+++ js/script.js
@@ -8,29 +8,20 @@ function generateSquare()
     return quadrato
 }
 
-function generateRandomNumber ( min, max){
+function randomNumberArray(min, max)
+{
 
-    let flag = false
-    let randomInt
-
-    while (!flag) {
-        randomInt = Math.floor(Math.random() * (max - min + 1)) + min
-        if(!bombs.includes(randomInt)){
-            flag = true
-        }
-    }
-    return randomInt
-}
-
-function createBombsArray(){
     let bombs = []
-    for(i = 0; i < 16 ; i++){
-        let random_number = generateRandomNumber(bombs, 1, number)
-        bombs.push(random_number)
+    let i = 0
+    while( i < 16){
+        let numberbomb = Math.floor(Math.random () * (max - min + 1)) + min
+        if(!bombs.includes(numberbomb)){ // se il numero  non presente in arraybombs allora pusho il numero all'interno di arraybombs fino a 16 volte
+            bombs.push(numberbomb)
+            i++
+        }
     }
 
     return bombs
-    
 }
 
 let arrayBombs = []
@@ -41,23 +32,31 @@ play.addEventListener('change', function(){
     const grid = document.getElementById('grind') // richiamo da html grid
     
     grid.innerHTML = ""
-    
     if (this.value == "100"){
+        arrayBombs = randomNumberArray(1, 100)
+        console.log(arrayBombs) // mettere fuori dal for sennò me lo genera infinite volte
+        
         for (let number  = 1 ; number <= 100 ; number++){
             
-            arrayBombs = createBombsArray(number)
-            console.log(arrayBombs)
             const box = generateSquare() // qua mi sono creato i vari square
             grid.appendChild(box) // creo un figlio all'interno dell'elemento che vado a selezionare
             box.innerHTML += number
             box.addEventListener('click', function(){
                 box.classList.add('yellow')
+                if(arrayBombs.includes(parseInt(this.innerHTML))){
+                    box.classList.remove('yellow')
+                    box.classList.add('red')
+                    grid.classList.add('blockevent')
+                }
                 console.log(number)
             })
             
         }
     }
-    else if(this.value == "81"){
+    else if(this.value == "81")
+    {
+        arrayBombs = randomNumberArray(1, 81)
+        console.log(arrayBombs) 
         for (let number =1 ; number <= 81 ; number++){
         
             const box = generateSquare() // qua mi sono creato i vari square
@@ -66,12 +65,20 @@ play.addEventListener('change', function(){
             box.classList.add('medium')
             box.addEventListener('click', function(){
                 box.classList.add('yellow')
+                if(arrayBombs.includes(parseInt(this.innerHTML))){
+                    box.classList.remove('yellow')
+                    box.classList.add('red')
+                    grid.classList.add('blockevent')
+                }
                 console.log(number)
             })
             
         }
     }
     else{
+
+        arrayBombs = randomNumberArray(1, 49)
+        console.log(arrayBombs) 
         for (let number = 1 ; number <= 49 ; number++){
         
             const box = generateSquare() // qua mi sono creato i vari square
@@ -80,6 +87,11 @@ play.addEventListener('change', function(){
             box.classList.add('hard')
             box.addEventListener('click', function(){
                 box.classList.add('yellow')
+                if(arrayBombs.includes(parseInt(this.innerHTML))){
+                    box.classList.remove('yellow')
+                    box.classList.add('red')
+                    grid.classList.add('blockevent')
+                }
                 console.log(number)
             })
             
